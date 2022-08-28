5bcfe948ca9b6d4f60e47697a74bd6c35e0c8d86
Michael Holod <git_ara@ukr.net>
2022-08-29 01:03:12 +0300

    The basic layout of the Main page has been prepared

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,5 +1,20 @@
+import { GiWorld } from 'react-icons/gi';
+
 export const Main:React.FunctionComponent = ()=>{
     return (<>
-        <h3>Main page</h3>
+        <section className='flex flex-col items-center justify-center bg-sky-50'>
+            <GiWorld />
+            <p>You can create your scheduled tasks</p>
+            <p>Share your tasks progress with your friends in chat</p>
+        </section>
+        <div className='w-full flex'>
+            <section className='w-2/4 bg-sky-300'>
+                <p>Completed tasks</p>
+                <p>Not completed tasks</p>
+            </section>
+            <section className='w-2/4 bg-amber-100'>
+                Chat statistics
+            </section>
+        </div>
     </>)
 }
